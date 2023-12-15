8b269bb5925887249b6c23ae99f7ff7304a308be
MifixGG <fed-dmitriy45@yandex.ru>
2023-12-15 11:06:46 +0300

    исправил несколько багов.

--- gesture.py
+++ gesture.py
@@ -1,10 +1,10 @@
 # import necessary packages
 
-import cv2
-import numpy as np
-import mediapipe as mp
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+# import cv2
+# import numpy as np
+# import mediapipe as mp
+# import tensorflow as tf
+# from tensorflow.keras.models import load_model
 
 # initialize mediapipe
 
